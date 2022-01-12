550138710b2a75f054b3cc148f33908525125449
tofarati <tofarati.agbaje@gmail.com>
2022-01-12 20:02:16 +0100

    Error Handling

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ app.prepare().then(() => {
     }));
     server.use(flash());
     
+    try{
     server.use(session({
         secret: process.env.SESSION_SECRET || 'secret',
         cookie: {
@@ -40,6 +41,17 @@ app.prepare().then(() => {
         mongoUrl: process.env.MONGODB_URI
         })
     }));
+    } catch (err) {
+        server.use(session({
+            secret: process.env.SESSION_SECRET || 'secret',
+            cookie: {
+                maxAge: 1000 * 60 * 60 * 24 * 7,
+                sameSite: true
+            },
+            resave: true,
+            saveUninitialized: false
+        }));
+    }
 
     server.set('views', __dirname + '/views');
     server.set('view engine', 'jsx');
@@ -70,26 +82,23 @@ app.prepare().then(() => {
     
     server.listen(process.env.PORT, () => console.log('Server listening on port ' + process.env.PORT));
     
-    mongoose.set("bufferCommands", false);
-    
-    mongoose.connect (
+    try {
+        mongoose.set("bufferCommands", false);
+        await mongoose.connect (
         process.env.MONGODB_URI,
         {
             ssl: true,
             useNewUrlParser: true,
             useUnifiedTopology: true
-        },
-        (err) => {
-            if (err) {
-                console.error("Error connecting to db: ", err);
-            } else {
-                console.log(`Connected to MongoDB`);
-            }
-        }
-    );
-    mongoose.connection.on('error', err => {
-        console.error(err);
-    })
+        });
+        console.log(`Connected to MongoDB`);
+        mongoose.connection.on('error', err => {
+            console.error(err);
+        })
+    } 
+    catch (err) {
+        console.error("Failed to connect to DB: "+ err)        
+    }
 })
 .catch((ex) => {
     console.error(ex.stack)
